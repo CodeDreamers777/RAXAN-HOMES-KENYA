cb2cfe23b3954a7e4a1e2a336f23884881a1afec
hacker777-cris <crispusgikonyo@gmail.com>
2024-09-08 17:46:39 +0300

    add properties_categories

--- api/views.py
+++ api/views.py
@@ -193,7 +193,7 @@ class PropertyViewSet(viewsets.ViewSet):
         return Response(serializer.data)
 
     def create(self, request):
-        property_type = request.data.get("base_property_type")
+        property_type = request.data.get("property_category")
         if property_type == "rental":
             serializer = RentalPropertySerializer(data=request.data)
         elif property_type == "sale":
