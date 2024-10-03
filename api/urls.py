790f1bec0e2a6d610a99f882bb8be01dae4fcdf2
hacker777-cris <crispusgikonyo@gmail.com>
2024-10-03 04:36:04 +0300

    fix url issue

--- api/urls.py
+++ api/urls.py
@@ -23,9 +23,9 @@ urlpatterns = [
     path(
         "confirm-booking/", views.ConfirmBookingView.as_view(), name="confirm-booking"
     ),
-    path("api/v1/bookings/", views.BookingListView.as_view(), name="booking-list"),
+    path("bookings/", views.BookingListView.as_view(), name="booking-list"),
     path(
-        "api/v1/bookings/<int:pk>/",
+        "bookings/<int:pk>/",
         views.BookingDetailView.as_view(),
         name="booking-detail",
     ),
