eece782f0c7fd1f175a90d20730bbfc68f51920f
hacker777-cris <crispusgikonyo@gmail.com>
2024-10-03 07:36:18 +0300

    host bookings correction

--- api/views.py
+++ api/views.py
@@ -589,7 +589,6 @@ class HostBookingsView(generics.ListAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        # Return bookings for properties owned by the user
         return Booking.objects.filter(property__host=self.request.user.profile)
 
 
@@ -598,7 +597,6 @@ class HostBookingDetailView(generics.RetrieveAPIView):
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
-        # Return bookings for properties owned by the user
         return Booking.objects.filter(property__host=self.request.user.profile)
 
     def retrieve(self, request, *args, **kwargs):
