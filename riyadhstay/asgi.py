4e9625bd562881ac6e8f4a3379c58eb551c13e51
hacker777-cris <crispusgikonyo@gmail.com>
2025-03-26 21:51:42 +0300

    update: realtime notifications

--- riyadhstay/asgi.py
+++ riyadhstay/asgi.py
@@ -1,16 +1,17 @@
-"""
-ASGI config for riyadhstay project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
-"""
-
 import os
-
 from django.core.asgi import get_asgi_application
+from channels.routing import ProtocolTypeRouter
+from api.utils.routing import application as routing_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "riyadhstay.settings")
 
-application = get_asgi_application()
+# Get the Django WSGI application
+django_application = get_asgi_application()
+
+# Create a combined application that handles both HTTP and WebSocket
+application = ProtocolTypeRouter(
+    {
+        "http": django_application,
+        "websocket": routing_application,
+    }
+)
