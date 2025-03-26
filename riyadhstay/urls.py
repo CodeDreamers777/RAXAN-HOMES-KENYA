2a900b526cc4e018effa09d4a909f519dd82e082
hacker777-cris <crispusgikonyo@gmail.com>
2025-03-26 21:13:56 +0300

    update: realtime notifications

--- riyadhstay/urls.py
+++ riyadhstay/urls.py
@@ -2,9 +2,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from channels.routing import get_default_application
+
 
 urlpatterns = [path("admin/", admin.site.urls), path("api/v1/", include("api.urls"))]
-
-# Ensure this is set for Channels
-application = get_default_application()
